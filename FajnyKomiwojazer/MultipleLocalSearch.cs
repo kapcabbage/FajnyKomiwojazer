e999d3007584175a5f958ddf1f729a6402b7abee
student <student@student.student>
2017-11-28 19:10:08 +0100

    MultipleLocalSearch added

---
+++ FajnyKomiwojazer/MultipleLocalSearch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FajnyKomiwojazer
+{
+    public class MultipleLocalSearch
+    {
+        private Graf Graf;
+
+        public MultipleLocalSearch(Graf graf)
+        {
+            Graf = graf;
+        }
+
+        public Graf Compute()
+        {
+            Graf computed = new Graf();
+
+            RandomCycle baseAlg = new RandomCycle(Graf);
+            var results = new double[Graf.Wierzcholki.Count];
+            for (int i = 0; i < Graf.Wierzcholki.Count; i++)
+            {
+                Graf gc = baseAlg.Compute();
+                LocalSearch localSearch = new LocalSearch(Graf, gc);
+                Graf ls = localSearch.Solve();
+                results[i] = gc.GetValueSoFarByEdge() - gc.GetDistanceSoFarByEdge();
+                if (results.Max() <= results[i])
+                {
+                    computed = ls;
+                }
+            }
+            var maxIndex = results.ToList().IndexOf(results.Max());
+
+            return computed;
+        }
+    }
+}
